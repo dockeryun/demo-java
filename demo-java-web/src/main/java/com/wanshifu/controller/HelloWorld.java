dc2b673fa2b3e6b4e7b8e3bdccec363e97dd4c86
huangming <huangming@wshifu.com>
2020-11-26 14:27:27 +0800

    项目结构调整

--- demo-java-web/src/main/java/com/wanshifu/controller/HelloWorld.java
+++ demo-java-web/src/main/java/com/wanshifu/controller/HelloWorld.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @param null
+ * @param
  * @author huangming@wshifu.com
  * @return
  * @date 2019/8/22  14:32
