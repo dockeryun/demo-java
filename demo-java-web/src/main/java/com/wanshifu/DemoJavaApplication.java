a105f118371ce5e0bd166f175fc38570552641bc
huangming <huangming@wshifu.com>
2020-11-26 14:36:11 +0800

    接入Apollo

--- demo-java-web/src/main/java/com/wanshifu/DemoJavaApplication.java
+++ demo-java-web/src/main/java/com/wanshifu/DemoJavaApplication.java
@@ -1,9 +1,12 @@
 package com.wanshifu;
 
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@EnableApolloConfig
 @SpringBootApplication
 public class DemoJavaApplication {
 
