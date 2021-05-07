a72d56a4b9e5aceb532a1e59cb65e0d3c795cde4
yangqiang <yangqiang@wshifu.com>
2021-05-07 15:24:58 +0800

    add：demo test

---
+++ demo-java-web/src/main/java/com/wanshifu/controller/DemoTestController.java
@@ -0,0 +1,15 @@
+package com.wanshifu.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoTestController {
+
+    @RequestMapping("/test")
+    public String helloWorld(){
+        return "Hello my name is yangqiang";
+    }
+
+}
