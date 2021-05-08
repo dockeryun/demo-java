db8812d55cbe43d0594d9ba16fe1835fdfd202a4
pengyu <pengyu@wshifu.com>
2021-05-08 13:44:19 +0800

    test package

---
+++ demo-java-api/src/main/java/com/wanshifu/entity/UserTest.java
@@ -0,0 +1,14 @@
+package com.wanshifu.entity;
+
+import lombok.Data;
+
+@Data
+public class UserTest {
+
+    private long id;
+    private int age;
+    /**
+     * name
+     */
+    private String name;
+}
