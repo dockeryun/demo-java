9c704abed789fd62a568bc9775506f8760e647b8
pengyu <pengyu@wshifu.com>
2021-05-10 16:42:48 +0800

    test package

--- demo-java-web/src/main/java/com/wanshifu/controller/DemoTestController.java
+++ demo-java-web/src/main/java/com/wanshifu/controller/DemoTestController.java
@@ -1,11 +1,15 @@
 package com.wanshifu.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/demo")
 public class DemoTestController {
+
+    @Value("${test.config}")
+    private String testConfig;
 
     @RequestMapping("/test")
     public String helloWorld(){
@@ -14,6 +18,7 @@ public class DemoTestController {
 
     @RequestMapping("/test/name")
     public String testHelloWorld(){
+        System.out.println(testConfig);
         return "Hello my name is name";
     }
 
